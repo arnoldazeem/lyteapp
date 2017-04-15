5994c6ae4ac37ccb66bae34452241009fd4e513b
Arnold AZ <arnoldazeem2008@gmail.com>
2017-04-15 18:02:45 +0200

    initial

---
+++ app/src/main/java/com/lyte/adaboo/lyteapp/StartActivity.java
@@ -0,0 +1,17 @@
+package com.lyte.adaboo.lyteapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adaboo on 4/14/17.
+ */
+
+public class StartActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
---
+++ app/src/main/java/com/lyte/adaboo/lyteapp/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.lyte.adaboo.lyteapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+
+
+/**
+ * Created by nirmal on 12/08/15.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private Context _context;
+    public static int totalPage = 3;
+
+    public ViewPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        _context = context;
+    }
+    @Override
+    public Fragment getItem(int position) {
+        Fragment f = new Fragment();
+        switch (position) {
+            case 0:
+                f = new ImageOneFragment();
+                break;
+            case 1:
+                f = new ImageTwoFragment();
+                break;
+            case 2:
+                f = new ImageThreeFragment();
+                break;
+        }
+        return f;
+    }
+    @Override
+    public int getCount() {
+        return totalPage;
+    }
+}
