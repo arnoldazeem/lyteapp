9ba6d558674c8dade5d839c54539c88528df2b4c
Arnold AZ <arnoldazeem2008@gmail.com>
2017-04-17 00:36:41 +0200

    facebook added

---
+++ app/src/main/java/com/lyte/adaboo/lyteapp/HomePage.java
@@ -0,0 +1,20 @@
+package com.lyte.adaboo.lyteapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adaboo on 4/16/17.
+ */
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_lay);
+
+    }
+}
--- app/src/main/java/com/lyte/adaboo/lyteapp/StartActivity.java
+++ app/src/main/java/com/lyte/adaboo/lyteapp/StartActivity.java
@@ -1,7 +1,25 @@
 package com.lyte.adaboo.lyteapp;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by adaboo on 4/14/17.
@@ -9,9 +27,77 @@ import android.support.v7.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity{
 
+    LoginButton fbLogin;
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //should be place exactly before setcontentView
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_start);
+
+        fbLogin = (LoginButton) findViewById(R.id.fblogin);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        fbLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Intent intent = new Intent(StartActivity.this, HomePage.class);
+                        startActivity(intent);
+
+                Toast.makeText(StartActivity.this, loginResult.getAccessToken().getUserId() + loginResult.getAccessToken().getToken(),
+                        Toast.LENGTH_LONG).show();
+
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+
+        });
+
+
+
+
+
+        /** was for generating hashes for facebook
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.lyte.adaboo.lyteapp",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("Your Tag", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+         */
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        Log.e("data",data.toString());
     }
 }
