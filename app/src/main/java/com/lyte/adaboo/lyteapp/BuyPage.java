464bd76c12092e4439bff4229ae7e0519fcdc040
Arnold AZ <arnoldazeem2008@gmail.com>
2017-04-24 23:11:05 +0200

    facebook onresume

--- app/src/main/java/com/lyte/adaboo/lyteapp/BuyPage.java
+++ app/src/main/java/com/lyte/adaboo/lyteapp/BuyPage.java
@@ -1,5 +1,6 @@
 package com.lyte.adaboo.lyteapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -12,6 +13,12 @@ import android.widget.Toast;
 
 public class BuyPage extends AppCompatActivity {
 
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+
+    String name;
+    String imageUrl;
+    int surname;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +30,25 @@ public class BuyPage extends AppCompatActivity {
         if (extras != null) {
 
 
-            Bundle inBundle = getIntent().getExtras();
-            String name = inBundle.get("name").toString();
-            String surname = inBundle.get("surname").toString();
-            String imageUrl = inBundle.get("imageUrl").toString();
+            SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+
+            String restoredText = prefs.getString("text", null);
+
+            if (restoredText != null) {
+                name = prefs.getString("name", "No name defined");//"No name defined" is the default value.
+                imageUrl = prefs.getString("imageUrl", "url");//"No name defined" is the default value.
+                surname = prefs.getInt("surname", 0); //0 is the default value.
+            }
+
+            //Bundle inBundle = getIntent().getExtras();
+            //String name = inBundle.get("name").toString();
+           // String surname = inBundle.get("surname").toString();
+           // String imageUrl = inBundle.get("imageUrl").toString();
+
+            //editor.clear();
+            //editor.commit(); // commit changes
+
+            new DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
 
             Toast.makeText(BuyPage.this, name + surname + imageUrl, Toast.LENGTH_LONG).show();
 
@@ -34,7 +56,7 @@ public class BuyPage extends AppCompatActivity {
             TextView nameView = (TextView)findViewById(R.id.nameAndSurname);
             nameView.setText("" + name + " " + surname);
 
-            new DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
+
           //  new BuyPage().DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
         }
 
--- app/src/main/java/com/lyte/adaboo/lyteapp/GraphApiTry.java
+++ app/src/main/java/com/lyte/adaboo/lyteapp/GraphApiTry.java
@@ -1,10 +1,13 @@
 package com.lyte.adaboo.lyteapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.AccessTokenTracker;
@@ -15,6 +18,7 @@ import com.facebook.FacebookRequestError;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -39,13 +43,26 @@ public class GraphApiTry extends AppCompatActivity {
     private URL profilePicture;
     private String userId;
     private String TAG = "GraphApiTry";
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         FacebookSdk.sdkInitialize(this);
-        setContentView(R.layout.graphtry);
+        setContentView(R.layout.activity_start);
+
+        TextView tx = (TextView) findViewById(R.id.allows);
+
+        TextView top = (TextView) findViewById(R.id.welcome);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "ARCHRISTY.ttf");
+        Typeface custom_font2 = Typeface.createFromAsset(getAssets(),  "ARESSENCE.ttf");
+
+
+        top.setTypeface(custom_font);
+        tx.setTypeface(custom_font2);
+
 
 
         callbackManager = CallbackManager.Factory.create();
@@ -63,17 +80,11 @@ public class GraphApiTry extends AppCompatActivity {
                 GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
-                        //Log.e(TAG,object.toString());
-                        //Log.e(TAG,response.toString());
-
-                        //Log.v("LoginActivity Response ", response.toString());
-
 
                         FacebookRequestError error = response.getError();
                         if (error != null) {
                             // handle your error
                             Log.e("Error", error + "");
-                            Toast.makeText(GraphApiTry.this, error.toString() +"", Toast.LENGTH_LONG).show();
                             return;
                         }
 
@@ -81,7 +92,9 @@ public class GraphApiTry extends AppCompatActivity {
                         try {
 
                             userId = object.getString("id");
+
                             profilePicture = new URL("https://graph.facebook.com/" + userId + "/picture?width=500&height=500");
+
                             if(object.has("first_name"))
                                 firstName = object.getString("first_name");
                             if(object.has("last_name"))
@@ -94,11 +107,22 @@ public class GraphApiTry extends AppCompatActivity {
                                 gender = object.getString("gender");
 
                             Intent main = new Intent(GraphApiTry.this, BuyPage.class);
-                            main.putExtra("name",firstName);
-                            main.putExtra("surname",lastName);
-                            main.putExtra("imageUrl",profilePicture.toString());
+                            //main.putExtra("name",firstName);
+                            //main.putExtra("surname",lastName);
+                            //main.putExtra("imageUrl",profilePicture.toString());
+
+                            SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                            editor.putString("name", firstName);
+                            editor.putString("surname", lastName);
+                            //editor.putInt("surname", lastName);
+                            editor.putString("imageUrl",profilePicture.toString());
+                            editor.commit();
+
+
+
                             startActivity(main);
                             finish();
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         } catch (MalformedURLException e) {
@@ -127,6 +151,36 @@ public class GraphApiTry extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, callback);
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Facebook login
+        Intent main = new Intent(GraphApiTry.this, BuyPage.class);
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("name", firstName);
+        editor.putString("surname", lastName);
+        //editor.putInt("surname", lastName);
+        editor.putString("imageUrl",profilePicture.toString());
+
+        editor.commit();
+
+        startActivity(main);
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        //Facebook login
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
